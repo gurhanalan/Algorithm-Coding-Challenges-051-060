1d67491f9431e572cb22c1c538cbdb410c6812cf
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-12-21 17:36:55 +0300

    Add challenge 51

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+"use strict";
 
+// 51. N-th Fibonacci - 6kyu
+/* I love Fibonacci numbers in general, but I must admit I love some more than others.
+
+I would like for you to write me a function that when given a number (n) returns the n-th number in the Fibonacci Sequence.
+
+For example:
+
+   nthFibo(4) == 2
+Because 2 is the 4th number in the Fibonacci Sequence.
+
+For reference, the first two numbers in the Fibonacci sequence are 0 and 1, and each subsequent number is the sum of the previous two. */
+
+function nthFibo(n) {
+    let current = 0;
+    const arr = [];
+    for (let i = 0; i < n; i++) {
+        arr.push(current);
+        current = (arr[arr.length - 1] || 1) + (arr[arr.length - 2] || 0);
+    }
+    return arr.pop();
+}
