4d9ccbefd644ebd60111fce3df0a920571c202a5
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-12-22 17:38:08 +0300

    Add challenge 52

--- script.js
+++ script.js
@@ -21,3 +21,20 @@ function nthFibo(n) {
     }
     return arr.pop();
 }
+
+// 52. Sum of positive - codewars - 8kyu
+/* 
+You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.
+ */
+
+function positiveSum(arr) {
+    return arr.reduce((acc, num) => {
+        if (num < 0) return 0 + acc;
+        else return acc + num;
+    }, 0);
+}
+console.log(positiveSum([-1, 2, 3, 4, -5]));
