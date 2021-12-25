b87b0aa6b590126c67adfb0f6a3039261d665b61
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-12-25 17:43:15 +0300

    Add challenge 55

--- script.js
+++ script.js
@@ -77,3 +77,25 @@ let codingRegex = /coding/; // Change this line
 let result = extractStr.match(codingRegex); // Change this line
 console.log(result);
 //["coding", index: 18, input: "Extract the word 'coding' from this string.", groups: undefined]
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// 55. IQ Test - codewars - 6kyu
+/* 
+Bob is preparing to pass IQ test. The most frequent task in this test is to find out which one of the given numbers differs from the others. Bob observed that one number usually differs from the others in evenness. Help Bob — to check his answers, he needs a program that among the given numbers finds one that is different in evenness, and return a position of this number.
+
+! Keep in mind that your task is to help Bob solve a real IQ test, which means indexes of the elements start from 1 (not 0)
+
+Examples:
+iqTest("2 4 7 8 10") => 3 // Third number is odd, while the rest of the numbers are even
+
+iqTest("1 2 1 1") => 2 // Second number is even, while the rest of the numbers are odd */
+
+function iqTest(numbers) {
+    const num = numbers.split(" ");
+    const even = numbers.split(" ").filter((el) => el % 2 === 0);
+    const odd = numbers.split(" ").filter((el) => el % 2 === 1);
+
+    if (even.length === 1) return num.indexOf(even[0]) + 1;
+    if (odd.length === 1) return num.indexOf(odd[0]) + 1;
+}
+console.log(iqTest("1 2 1 1"));
