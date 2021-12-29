77d9da202c03da69cd4b4075237939e34f9bb24f
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-12-29 17:56:05 +0300

    Add challenge 58

--- script.js
+++ script.js
@@ -170,3 +170,23 @@ const uniqueInOrder4 = function (it) {
 };
 
 console.log(uniqueInOrder4("AAAABBBCCDAABBB"));
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// #############################################################
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// 58. Find the unique number - codewars - 6kyu
+/* There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+It’s guaranteed that array contains at least 3 numbers.
+
+The tests contain some very huge arrays, so think about performance.
+ */
+function findUniq(arr) {
+    let same = arr[0] === arr[1] ? arr[0] : arr[0] === arr[2] ? arr[0] : arr[1];
+    for (let num of arr) {
+        if (num !== same) return num;
+    }
+}
+console.log(findUniq([0, 0, 0.55, 0, 0]));
